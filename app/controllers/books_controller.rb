e8d9ff7ee856740c23eacf4a1226fcd55a6f3513
Sphaerus <Kuba@jakubs-MacBook-Pro.local>
2013-10-10 11:11:14 +0200

    dodawanie książek

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  
+  def create
+    @book = Book.new(book_params)
+    respond_to do |format|
+      if @book.save
+        format.html {redirect_to root_path}
+      else
+        format.html {redirect_to root_path}
+      end
+    end      
+  end   
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:isbn, :title, :author, :publishing_date)
+  end  
+end  
