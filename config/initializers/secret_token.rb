968afa4ebb43e4d13efae535894f45cb8c92f61b
Sphaerus <Kuba@jakubs-MacBook-Pro.local>
2013-10-10 10:10:04 +0200

    nowa aplikacja

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wyszukiwarka::Application.config.secret_key_base = '23998a6e46b9ee491e49ec830487028fb2e9f53e42d7aee328de37ccea19ceccd6b83aa649a60247570fad31554ab8839b64cbf86eb0aa1ef70e21c602cbb48c'
