76e8b9962e457fedb462e83a9f2d155258de254e
Sphaerus <wyfsm1@gmail.com>
2013-12-01 09:26:50 +0100

    validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+  validates :title, :author, :publishing_date, :isbn, presence: true
   
   def self.browse(query)
     self.where("title REGEXP :query OR author REGEXP :query OR publishing_date REGEXP :query OR isbn REGEXP :query", query: query) 
